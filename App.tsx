9dab9a03dd08eb4632d0d36105e87ec2e620513f
Sarah Spicer <spicerfairies@gmail.com>
2021-11-17 16:18:57 -0600

    Screen Navigation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
-import MapView, { Marker, AnimatedRegion, Polyline } from 'react-native-maps';
-import { StyleSheet, Text, View, Dimensions, TouchableOpacity } from 'react-native';
+import MapView from 'react-native-maps';
+import { StyleSheet, Text, View, Dimensions, TouchableOpacity, Button } from 'react-native';
 import * as Location from 'expo-location';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-export default function App() {
+
+function homeScreen({navigation}) {
   // Default coordaninates upon loading (Camp Allen).
   const [location, setLocation] = useState({
     latitude: 30.24166,
@@ -36,39 +39,80 @@ export default function App() {
   }
 
 
+  return ( 
+      <View style={styles.container}>
+        <MapView 
+        style={styles.map}
+        initialRegion={location}
+        showsUserLocation={true}>
+        </MapView>
+        <StatusBar style="auto" />
+        <Text style={styles.paragraph}>{text}</Text>
+          
+        <View style={{position: 'absolute'}}>
+          <Button title = "Edit Map" 
+          onPress = {() => navigation.navigate("Edit Map")}
+           />
+        </View>
+      </View>
+  );
+}
+
+function editScreen() {
+    // Default coordaninates upon loading (Camp Allen).
+    const [location, setLocation] = useState({
+      latitude: 30.24166,
+      longitude: -95.95935,
+      latitudeDelta: 0.003,
+      longitudeDelta: 0.003,
+    });
+  
   return (
     <View style={styles.container}>
-      <MapView 
-      style={styles.map}
-      initialRegion={location}
-      showsUserLocation={true}>
+        <MapView 
+        style={styles.map}
+        initialRegion={location}
+        showsUserLocation={true}>
+        </MapView>
 
-      </MapView>
-      <StatusBar style="auto" />
-      <Text style={styles.paragraph}>{text}</Text>
+       <View style={styles.btnContainer}>
+          <TouchableOpacity
+            onPress= {() => alert('Yay! you can start mapping!')}
+            style={[styles.mapBtn, { backgroundColor:'green' }]}>
+            <Text style={styles.btnText}>Start Point</Text>
+          </TouchableOpacity>
 
-      <View style={styles.btnContainer}>
-        <TouchableOpacity
-          onPress= {() => alert('Yay! you can start mapping!')}
-          style={[styles.mapBtn, { backgroundColor:'green' }]}>
-          <Text style={styles.btnText}>Start Point</Text>
-        </TouchableOpacity>
+          <TouchableOpacity 
+            onPress= {() => alert('Congratulations, you compleated a trail! ')}
+            style={[styles.mapBtn, { backgroundColor:'red'}]}>
+            <Text style={styles.btnText}>Stop Point</Text>
+          </TouchableOpacity>
 
-        <TouchableOpacity 
-          onPress= {() => alert('Congratulations, you compleated a trail! ')}
-          style={[styles.mapBtn, { backgroundColor:'red'}]}>
-          <Text style={styles.btnText}>Stop Point</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity 
-          onPress= {() => alert('You will now be able to rewalk this trail any time you want.')}
-          style={[styles.mapBtn, {backgroundColor:'blue'}]}>
-          <Text style={styles.btnText}>Save</Text>
-        </TouchableOpacity>
-      </View>
+          <TouchableOpacity 
+            onPress= {() => alert('You will now be able to rewalk this trail any time you want.')}
+            style={[styles.mapBtn, {backgroundColor:'blue'}]}>
+            <Text style={styles.btnText}>Save</Text>
+          </TouchableOpacity>
+        </View>
     </View>
   );
 }
+
+// Screen Navigation
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Choose a Trail" component = {homeScreen} />
+        <Stack.Screen name = "Edit Map" component = {editScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
