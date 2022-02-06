f2ebb7c7bad2f88502fffeff174ad72f7d4347ef
clong desktop <chip_long@yahoo.com>
2022-02-06 13:55:03 -0700

    working on save trail

--- server/controllers/trailControllers.js
+++ server/controllers/trailControllers.js
@@ -2,6 +2,7 @@ const { body, validationResult } = require("express-validator");
 
 const { Trail, TrailPoints } = require("../models");
 
+// returns a list of all of the trails and the trail's points
 exports.listTrails = async (req, res) => {
   try {
     trails = await Trail.findAll({
@@ -13,3 +14,49 @@ exports.listTrails = async (req, res) => {
     res.status(500).json(err);
   }
 };
+
+exports.saveTrail = [
+  // Trail validation
+  body("name")
+    .trim()
+    .custom(async (value) => {
+      const trail = await Trail.findAll({ where: { name: value } });
+      if (trail.length > 0) {
+        throw new Error(`Trail name, "${value}", is already in use`);
+      }
+    }),
+  body("description").trim().escape().optional(),
+  body("difficulty", "Invalid selection for difficulty").isIn([
+    "easy",
+    "moderate",
+    "hard",
+  ]),
+  body("isClosed", "isClosed must be true/false").isBoolean().optional(),
+
+  // Trail Points validation
+  body("trailPoints.*", "The trail must have at least 2 points.").isArray({
+    min: 2,
+  }),
+  // body("trailPoints.*.latitude").exists(),
+  // body("trailPoints.*.longitude").exists(),
+
+  async (req, res) => {
+    try {
+      // handle validation errors
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        res.status(400).json(errors);
+        return;
+      }
+
+      const newData = req.body;
+
+      const trail = await Trail.create(newData);
+
+      res.status(201).json(trail);
+    } catch (err) {
+      console.log("saveTrail Catch Error:\n", err);
+      res.status(500).json(err);
+    }
+  },
+];
--- server/routes/api/trailRoutes.js
+++ server/routes/api/trailRoutes.js
@@ -1,6 +1,15 @@
 const router = require("express").Router();
-const { listTrails } = require("../../controllers/trailControllers");
+const { verifyToken } = require("../../utils/auth");
+
+const { listTrails, saveTrail } = require("../../controllers/trailControllers");
 
 router.get("/", listTrails);
+
+router.post("/", /* verifyToken,*/ saveTrail);
+
+// get "/:trailId", <-- returns all trail details (POIs, Hazards, etc.)
+// post "/:trailId/close", verifyToken,
+// post "/:trailID/add_hazard"
+// post "/:trailID/add_poi", verifyToken,
 
 module.exports = router;
