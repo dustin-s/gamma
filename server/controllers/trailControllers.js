498ac961d2e5dd8b692523131411f63aff9de63d
clong desktop <chip_long@yahoo.com>
2022-02-05 18:32:19 -0700

    added general list trails functionality

--- server/controllers/trailControllers.js
+++ server/controllers/trailControllers.js
@@ -1,7 +1,15 @@
 const { body, validationResult } = require("express-validator");
 
-const { Trail } = require("../models");
+const { Trail, TrailPoints } = require("../models");
 
-exports.listTrails = (req, res) => {
-  res.status(200).json({ trails: "trails" });
+exports.listTrails = async (req, res) => {
+  try {
+    trails = await Trail.findAll({
+      include: TrailPoints,
+    });
+    res.status(200).json(trails);
+  } catch (er) {
+    console.log("listTrails Catch Error:\n", err);
+    res.status(500).json(err);
+  }
 };
