a6b03e0f4e84c6795eb526c69036c8f2749aeffb
clong desktop <chip_long@yahoo.com>
2022-02-16 12:07:22 -0700

    refactored EditTrail -> TrailScreen and started to configure the screen to be not just edit trails

--- client/App.tsx
+++ client/App.tsx
@@ -2,8 +2,10 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 // Screens
 import HomeScreen from "./app/screens/HomeScreen";
-import EditScreen from "./app/screens/EditScreen";
 import AdminLogin from "./app/screens/AdminLogin";
+import PointOfInterest from "./app/screens/PointOfInterest";
+import GetPhoto from "./app/screens/GetPhoto";
+import TrailScreen from "./app/screens/TrailScreen";
 
 // Screen Navigation
 const Stack = createNativeStackNavigator();
@@ -14,14 +16,20 @@ function App() {
       <Stack.Navigator
         screenOptions={{
           headerStyle: {
-            backgroundColor: '#98002D',
-             },
-          headerTintColor: '#fff',
-        }}>
-        
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false}}/>
-        <Stack.Screen name="Admin" component={AdminLogin}/>
-        <Stack.Screen name="Edit Map" component={EditScreen}/>
+            backgroundColor: "#98002D",
+          },
+          headerTintColor: "#fff",
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="Admin" component={AdminLogin} />
+        <Stack.Screen name="Trail Screen" component={TrailScreen} />
+        <Stack.Screen name="Point of Interest" component={PointOfInterest} />
+        <Stack.Screen name="Get Photo" component={GetPhoto} />
       </Stack.Navigator>
     </NavigationContainer>
   );
