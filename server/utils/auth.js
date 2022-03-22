d460791dd3c91adefdbbf7218d9d8e8b550b86d5
clong desktop <chip_long@yahoo.com>
2022-03-22 16:17:04 -0600

    added logging

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,5 +1,8 @@
 const jwt = require("jsonwebtoken");
 
+const { loggers } = require("winston");
+const logger = loggers.get("logger");
+
 const SECRET = process.env.JWT;
 const EXPIRATION = "12h";
 
@@ -9,6 +12,10 @@ exports.signToken = ({ email, userId }) => {
 };
 
 exports.verifyToken = ({ req }) => {
+  logger.debug("verify token reached", {
+    controller: "auth.js --> verifyToken()",
+  });
+
   // allows token to be sent via req.body, req.query, or headers
   let token = req.body.token || req.query.token || req.headers.authorization;
 
@@ -23,8 +30,8 @@ exports.verifyToken = ({ req }) => {
   try {
     const { data } = jwt.verify(token, SECRET, { maxAge: EXPIRATION });
     req.user = data;
-  } catch {
-    console.log("Invalid token");
+  } catch (e) {
+    logger.error("Invalid token", { controller: "auth.js --> verifyToken()" });
   }
 
   return req;
