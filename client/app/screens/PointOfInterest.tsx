aa96b6307c7357a9831b05212f9b5356cd212616
Chip Long <chip_long@yahoo.com>
2022-05-02 16:05:08 -0600

    updated POI screent to accept most inputs (no photo yet)

--- client/app/screens/PointOfInterest.tsx
+++ client/app/screens/PointOfInterest.tsx
@@ -3,8 +3,14 @@
 // https://docs.expo.dev/versions/latest/sdk/camera/
 
 import { useContext, useState } from "react";
-import { Image, StyleSheet, Text, View } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import {
+  ImageBackground,
+  StyleSheet,
+  Switch,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
 import MapButton from "../components/MapButton";
 import { POIObj } from "../interfaces/POIObj";
 
@@ -12,6 +18,13 @@ import { POIObj } from "../interfaces/POIObj";
 import { StackNativeScreenProps } from "../interfaces/StackParamList";
 import { AuthContext } from "../utils/authContext";
 import { BASE_URL } from "../utils/constants";
+
+const getImage = (str: string | null) => {
+  if (str) {
+    return BASE_URL + str;
+  }
+  return null;
+};
 
 type POIScreenProps = StackNativeScreenProps<"Point of Interest">;
 
@@ -35,31 +48,155 @@ export default function PointOfInterest({ navigation, route }: POIScreenProps) {
   };
 
   const { auth } = useContext(AuthContext);
-  const [image, setImage] = useState(poiObj.image);
+  // stores the URI for the image
+  const [image, setImage] = useState(getImage(poiObj?.image));
+  const [editDesc, setEditDesc] = useState(poiObj.description === null);
+  const [description, setDescription] = useState(poiObj?.description);
+  const [isActive, setIsActive] = useState(true);
+  const [isDirty, setIsDirty] = useState(false);
 
-  const editPOI = () => {
-    console.log("Edit POI", poiObj.description);
-    navigation.navigate("Home");
+  const handleEditDesc = () => {
+    setEditDesc(true);
+  };
+
+  const handleCancelDesc = () => {
+    setDescription(poiObj?.description);
+    setEditDesc(false);
+    checkIsDirty();
+  };
+
+  const handleUpdateDesc = () => {
+    setEditDesc(false);
+    checkIsDirty();
+  };
+
+  const takePhoto = () => {
+    // do something
+  };
+
+  const handleSetIsActive = () => {
+    setIsActive(!isActive);
+    checkIsDirty();
+  };
+
+  const checkIsDirty = () => {
+    const photoChanged = getImage(poiObj.image) !== image;
+    const descChanged = poiObj.description !== description;
+    const isActiveChanged = poiObj.isActive !== isActive;
+
+    setIsDirty(photoChanged || descChanged || isActiveChanged);
+  };
+
+  const handleSave = () => {
+    if (!auth.isAuthenticated || !isDirty) {
+      navigation.navigate("Trail Screen");
+    }
+
+    // save...
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      {image ? (
-        <Image source={{ uri: BASE_URL + poiObj.image }} style={styles.image} />
-      ) : (
-        <Text>get image</Text>
+    <View style={styles.container}>
+      {editDesc && (
+        <View style={[styles.textContainer]}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Add a description"
+            value={description ? description : undefined}
+            onChangeText={(value) => setDescription(value)}
+            multiline
+            autoCapitalize="sentences"
+          />
+          <View style={styles.buttonContainer}>
+            <MapButton
+              label="Cancel"
+              backgroundColor="red"
+              handlePress={handleCancelDesc}
+            />
+            <MapButton
+              label="Commit"
+              backgroundColor="green"
+              handlePress={handleUpdateDesc}
+            />
+          </View>
+        </View>
       )}
-      <Text style={styles.text}>{poiObj.description}</Text>
+
+      {image ? (
+        <ImageBackground
+          source={{ uri: image }}
+          style={styles.image}
+          // resizeMethod="scale"
+        >
+          {auth.isAuthenticated && (
+            <View style={styles.button}>
+              <MapButton
+                label="Update Photo"
+                backgroundColor="blue"
+                handlePress={takePhoto}
+              />
+            </View>
+          )}
+        </ImageBackground>
+      ) : (
+        <View style={styles.imageContainer}>
+          <View style={styles.button}>
+            <MapButton
+              label="Add Photo"
+              backgroundColor="blue"
+              handlePress={takePhoto}
+            />
+          </View>
+        </View>
+      )}
+
+      {!editDesc && (
+        <View style={styles.textContainer}>
+          <Text style={styles.text}>{description}</Text>
+          {auth.isAuthenticated && (
+            <View style={styles.button}>
+              <MapButton
+                label="Edit"
+                backgroundColor="blue"
+                handlePress={handleEditDesc}
+              />
+            </View>
+          )}
+        </View>
+      )}
+
       {auth.isAuthenticated && (
-        <View style={styles.button}>
-          <MapButton
-            label="Edit"
-            backgroundColor="blue"
-            handlePress={editPOI}
+        <View style={styles.controlGroup}>
+          <Text style={styles.label}>Active?</Text>
+          <Switch
+            trackColor={{ false: "#767577", true: "#81b0ff" }}
+            thumbColor={isActive ? "#5fd455" : "#f4f3f4"}
+            ios_backgroundColor="#3e3e3e"
+            onValueChange={handleSetIsActive}
+            value={isActive}
           />
         </View>
       )}
-    </SafeAreaView>
+
+      {console.log("isDirty:", isDirty)}
+      {!isDirty ? (
+        <View style={styles.buttonContainer}>
+          <MapButton
+            label="Close"
+            backgroundColor="blue"
+            handlePress={handleSave}
+          />
+        </View>
+      ) : (
+        <View style={styles.buttonContainer}>
+          <MapButton
+            label="Save and Close"
+            backgroundColor="red"
+            handlePress={handleSave}
+          />
+        </View>
+      )}
+    </View>
   );
 }
 
@@ -67,25 +204,59 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#eee",
-    alignItems: "flex-start",
+    alignItems: "center",
     justifyContent: "flex-start",
     height: "100%",
   },
+  button: {
+    position: "absolute",
+    right: 10,
+    bottom: 5,
+  },
+
+  // when no image
+  imageContainer: {
+    height: 300,
+    width: 400,
+  },
   image: {
-    height: "50%",
-    width: "100%",
-    resizeMode: "center",
+    height: 300,
+    width: 400, //height*1.3333,
+    resizeMode: "contain",
     alignSelf: "flex-start",
   },
+
+  textContainer: {
+    paddingTop: 10,
+    flexGrow: 1,
+    width: 400,
+  },
   text: {
-    paddingHorizontal: 10,
+    paddingHorizontal: 15,
     color: "black",
     // fontSize: 20,
     overflow: "scroll",
   },
-  button: {
-    position: "absolute",
-    right: 5,
-    bottom: 5,
+  textInput: {
+    borderWidth: 1,
+    flexGrow: 1, // fill the available space
+    textAlignVertical: "top",
+    padding: 5,
+    margin: 10,
+  },
+  buttonContainer: {
+    padding: 5,
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+  },
+
+  controlGroup: {
+    flexDirection: "row",
+    alignItems: "center",
+    padding: 5,
+  },
+  label: {
+    fontSize: 20,
+    marginRight: 5,
   },
 });
