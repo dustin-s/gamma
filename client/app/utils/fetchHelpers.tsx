ac2786823f2a06ecf2f36ff5f7b4bea4ee9371a0
clong desktop <chip_long@yahoo.com>
2022-06-12 10:03:42 -0600

    commented out the fetchImageFromURI method to ensure it is not used

--- client/app/utils/fetchHelpers.tsx
+++ client/app/utils/fetchHelpers.tsx
@@ -1,27 +1,26 @@
 import { POIObj } from "../interfaces/POIObj";
-import { Authentication } from "./authContext";
 import { BASE_API } from "./constants";
 
 // this little gem is from: https://github.com/expo/examples/blob/master/with-aws-storage-upload/App.js
-export const fetchImageFromUri = async (uri: string) => {
-  console.log("*** Start Fetch Image From Uri ***\n");
+// export const fetchImageFromUri = async (uri: string) => {
+//   console.log("*** Start Fetch Image From Uri ***\n");
 
-  // uri =
-  //   "https://file-examples.com/storage/feb8f98f1d627c0dc94b8cf/2017/10/file_example_JPG_100kB.jpg";
-  const response = await fetch(uri);
-  // console.log("response:");
-  // console.log(response);
+//   // uri =
+//   //   "https://file-examples.com/storage/feb8f98f1d627c0dc94b8cf/2017/10/file_example_JPG_100kB.jpg";
+//   const response = await fetch(uri);
+//   // console.log("response:");
+//   // console.log(response);
 
-  const blob = await response.blob();
-  console.log("uri:", uri);
-  console.log("blob (on next line):");
-  console.log(JSON.stringify(blob));
-  console.log("Blob.prototype.size:", blob.size, "(bytes)");
-  console.log("Blob.prototype.type:", blob.type);
+//   const blob = await response.blob();
+//   console.log("uri:", uri);
+//   console.log("blob (on next line):");
+//   console.log(JSON.stringify(blob));
+//   console.log("Blob.prototype.size:", blob.size, "(bytes)");
+//   console.log("Blob.prototype.type:", blob.type);
 
-  console.log("\n*** End Fetch Image From Uri ***");
-  return blob;
-};
+//   console.log("\n*** End Fetch Image From Uri ***");
+//   return blob;
+// };
 
 const difference = (
   newObj: Record<string, any>,
