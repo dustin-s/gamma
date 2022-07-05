8eebb3da9b7faac8aa1a67a961f3e0f84d5f853a
clong desktop <chip_long@yahoo.com>
2022-07-04 20:33:06 -0600

    added useTrailContext

---
+++ client/app/hooks/useTrailContext.ts
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { initialStateType, TrailContext } from "../contexts/TrailContext";
+
+// this is mainly being used to spread the TrailContext
+export const useTrailContext = () => {
+  const context = useContext(TrailContext);
+
+  // This isn't really needed since the navigator doesn't let us put this on individual screens
+  if (!context) {
+    throw Error(
+      "You must be within the TrailContextProvider to use TrailContext."
+    );
+  }
+
+  // destructure the context so we can use the values directly, TS doesn't let us just spread the state.
+  const { trailId, trailData, locationArr, poiArr }: initialStateType =
+    context.trailState;
+
+  return {
+    trailId,
+    trailData,
+    locationArr,
+    poiArr,
+    trailDispatch: context.trailDispatch,
+  };
+};
