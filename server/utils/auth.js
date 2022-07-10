35e25e899bd9a0efa3a0db61e5eb410bd68a7cbe
clong desktop <chip_long@yahoo.com>
2022-07-09 22:56:30 -0600

    updated auth to return standard error messages (JSON)

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -23,7 +23,7 @@ exports.verifyToken = (req, res, next) => {
     logger.error("No token", {
       controller: "auth.js --> verifyToken()",
     });
-    res.status(400).json("Token not present");
+    res.status(400).json({ error: "Token not present" });
     return;
   }
 
@@ -41,6 +41,6 @@ exports.verifyToken = (req, res, next) => {
       controller: "auth.js --> verifyToken()",
       errorMsg: "Invalid token",
     });
-    res.status(403).json("Token invalid");
+    res.status(403).json({ error: "Token invalid" });
   }
 };
